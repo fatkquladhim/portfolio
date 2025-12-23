4a8a9040e0768534f808018e201b743a91564fc5
adhim <fatquladhim@gmail.com>
2025-12-24 00:13:29 +0700

    fix:auth-client.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://portfolio-api-production-5604.up.railway.app/api',
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 });
 
--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,10 +1,10 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: import.meta.env.VITE_API_BASE_URL ? `${import.meta.env.VITE_API_BASE_URL}/auth` : "https://portfolio-api-production-5604.up.railway.app/api",
+    baseURL: import.meta.env.VITE_AUTH_URL,
     fetchOptions: {
         credentials: "include"
     }
-})
+});
 
 export const { signIn, signUp, useSession, signOut } = authClient;
