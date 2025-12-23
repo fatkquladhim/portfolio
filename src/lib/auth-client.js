d504ef9ad497e828fabf2c44385380e3b2ed22cf
adhim <fatquladhim@gmail.com>
2025-12-23 23:12:49 +0700

    fit:env

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: import.meta.env.VITE_API_BASE_URL ? `${import.meta.env.VITE_API_BASE_URL}/auth` : "http://localhost:3001/api/auth",
+    baseURL: import.meta.env.VITE_API_BASE_URL ? `${import.meta.env.VITE_API_BASE_URL}/auth` : "https://portfolio-api-production-5604.up.railway.app/api",
 })
 
 export const { signIn, signUp, useSession, signOut } = authClient;
